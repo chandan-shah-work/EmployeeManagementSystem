9f3c72eb08343fe3e09f96813046b8281bf489cb
chandan-shah-project <work.with.chandan95@gmail.com>
2025-03-02 18:54:39 +0530

    Add project files.

---
+++ EmployeeManagement.API/DTOs/SaveEmployeeRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.API.DTOs
+{
+    public class SaveEmployeeRequest
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Designation { get; set; }
+        public int? ReportsToId { get; set; } // Manager's ID (nullable)
+        public List<AddressDto> Addresses { get; set; } // List of addresses
+    }
+
+    public class AddressDto
+    {
+        public string City { get; set; }
+        public string Area { get; set; }
+        public string PinCode { get; set; }
+    }
+}
---
+++ EmployeeManagement.API/Repository/Implementations/EmployeeRepository.cs
@@ -0,0 +1,70 @@
+﻿using EmployeeManagement.API.DBContext;
+using EmployeeManagement.API.Models;
+using EmployeeManagement.API.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.API.Repository.Implementations
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly EmployeeDbContext _context;
+
+        public EmployeeRepository(EmployeeDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Employee> SaveEmployeeAsync(Employee employee)
+        {
+            _context.Employees.Add(employee);
+            await _context.SaveChangesAsync();
+            return employee;
+        }
+
+        public async Task UpdateAddressAsync(Address address)
+        {
+            var existingAddress = await _context.Addresses.FirstOrDefaultAsync(a => a.Id == address.Id);
+
+            if (existingAddress == null)
+            {
+                throw new KeyNotFoundException("Address not found.");
+            }
+
+            // Update the fields
+            existingAddress.City = address.City;
+            existingAddress.Area = address.Area;
+            existingAddress.PinCode = address.PinCode;
+            existingAddress.EmployeeId = address.EmployeeId; // Ensure EmployeeId is updated
+
+            _context.Addresses.Update(existingAddress);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<Address>> GetAddressesByEmployeeIdAsync(int employeeId)
+        {
+            return await _context.Addresses
+                .Where(a => a.EmployeeId == employeeId)
+                .ToListAsync();
+        }
+
+        public async Task<List<Employee>> GetEmployeesByManagerIdAsync(int managerId)
+        {
+            return await _context.Employees
+                .Where(e => e.ReportsToId == managerId)
+                .ToListAsync();
+        }
+
+        public async Task<Employee> GetManagerByEmployeeIdAsync(int employeeId)
+        {
+            var employee = await _context.Employees
+                .Include(e => e.ReportsTo)
+                .FirstOrDefaultAsync(e => e.Id == employeeId);
+            return employee?.ReportsTo;
+        }
+    }
+}
